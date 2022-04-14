b57fe0243f3b429a92d23f16a42daaa0844b3bf6
Tusman917 <tusman2018@gmail.com>
2022-04-14 16:32:20 -0400

    https://github.com/tusman917/LearnInBackEnd.git

--- learning/src/components/table/columns.js
+++
@@ -1,6 +0,0 @@
-export const columns = [
-  { label: "Date", field: "date" },
-  { label: "Company Name", field: "company" },
-  { label: "Amount", field: "amount" },
-  { label: "Status", field: "status" },
-];
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const creditCardData = require("./data/creditCard");
+const plbData = require("./data/personalLearningBudget");
+const transactionsData = require("./data/transactions");
+
+const PORT = 9999;
+app.use(cors());
+// app.use(express.static('public'));
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+*/
+
+app.get("/transactions", (_, res) => {
+  res.send(transactionsData);
+});
+
+app.get("/card-details", (req, res) => {
+  const showCardNumber = req.query["show-card-number"] === "true";
+  res.send(creditCardData(showCardNumber));
+});
+
+app.get("/personal-learning-budget", (_, res) => {
+  res.send(plbData);
+});
+
+/*
+|--------------------------------------------------------------------------
+| Server Listening
+|--------------------------------------------------------------------------
+*/
+
+app.listen(PORT, () => {
+  console.log(`Transactions server is listening on port ${PORT}!`);
+});
